9f71bb09f18e606299c76e13ae2fc4d0fb8ae68b
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-13 22:33:10 +0600

    useRef hook

---
+++ src/MyComponent.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect, useRef } from 'react';
+
+function MyComponent(){
+
+  let [number, setNumber] = useState(0);
+
+  return(
+  <button>
+    Click Me
+  </button>)
+}
+export default MyComponent;
