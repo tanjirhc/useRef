ab45735a038750942f4ec2809c50254216b1e54d
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-13 22:57:30 +0600

    style color changed

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -2,23 +2,45 @@ import { useState, useEffect, useRef } from 'react';
 
 function MyComponent(){
 
-  const inputRef = useRef(null);
+  const inputRef1 = useRef(null);
+  const inputRef2 = useRef(null);
+  const inputRef3 = useRef(null);
 
   useEffect(() => {
     console.log("Component Rendered");
-    console.log(inputRef);
   })
 
-  function handleClick(){
-    inputRef.current.focus();
+  function handleClick1(){
+    inputRef1.current.focus();
+    inputRef1.current.style.backgroundColor = "yellow";
+  }
+
+  function handleClick2(){
+    inputRef2.current.focus();
+    inputRef2.current.style.backgroundColor = "magenta";
+  }
+
+  function handleClick3(){
+    inputRef3.current.focus();
+    inputRef3.current.style.backgroundColor = "aqua";
   }
 
   return(
   <div>
-    <button onClick={handleClick}>
-      Click Me
+    <button onClick={handleClick1}>
+      Click Me 1
     </button>
-    <input ref={inputRef} />
+    <input ref={inputRef1} /> <br/>
+
+    <button onClick={handleClick2}>
+      Click Me 2
+    </button>
+    <input ref={inputRef2} /> <br/>
+
+    <button onClick={handleClick3}>
+      Click Me 3
+    </button>
+    <input ref={inputRef3} />
   </div>);
 }
 export default MyComponent;
