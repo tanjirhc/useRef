7652d40a5a7dc2abef7312e6ff3dc1d9a876d62a
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-13 22:36:01 +0600

    component rendered

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -4,8 +4,16 @@ function MyComponent(){
 
   let [number, setNumber] = useState(0);
 
+  useEffect(() => {
+    console.log("Component Rendered");
+  })
+
+  function handleClick(){
+    setNumber(n => n + 1);
+  }
+
   return(
-  <button>
+  <button onClick={handleClick}>
     Click Me
   </button>)
 }
